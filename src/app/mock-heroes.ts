1209eae3928d23d9c4acbd4f45a603104f35b769
peykov <peykovn@gmail.com>
2022-09-19 17:26:08 -0300

    part 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  { id: 1, name: "Windstorm" },
+  { id: 2, name: "Ra" },
+  { id: 3, name: "Seafury" },
+  { id: 4, name: "Rockthrower" },
+  { id: 5, name: "Flashlight" },
+  { id: 6, name: "Nvimer" },
+]
